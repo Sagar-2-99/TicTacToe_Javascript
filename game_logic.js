67161b633dfe1f0da437d2486c0ff9a21821e6f9
Sagar-2-99 <sagardwevedi475@gmail.com>
2023-12-31 16:13:19 +0530

    code based added

---
+++ game_logic.js
@@ -0,0 +1,83 @@
+let playerText = document.getElementById('playerText');
+let restartBtn = document.getElementById('restartBtn');
+let boxes = Array.from(document.getElementsByClassName('box'));
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks');
+
+console.log(boxes);
+
+const O_TEXT="O";
+const X_TEXT="X";
+let currentPlayer = X_TEXT;
+let spaces = Array(9).fill(null);
+let winner = false;
+console.log(spaces);
+
+const startGame = ()=>{
+    boxes.forEach(box => box.addEventListener('click',boxClicked));
+}
+
+const endGame = () => {
+    boxes.forEach(box => box.removeEventListener("click", boxClicked));
+}
+
+function boxClicked(e){
+    console.log(e.target);
+    const id = e.target.id;
+    console.log(id);
+    if(winner) {
+        return;
+    }
+    if(!spaces[id]){
+        spaces[id]=currentPlayer
+        e.target.innerText = currentPlayer;
+
+        if(palyerHasWon()!==false){
+            winner = true;
+            playerText = `${currentPlayer} has won!!!`;
+            let winning_blocks = palyerHasWon()
+            console.log(winning_blocks)
+            winning_blocks.map(box => boxes[box].style.backgroundColor=winnerIndicator);
+            // endGame();
+            return;
+        } else {
+            winner = false;
+        }
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+    }
+}
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [0,3,6],
+    [6,7,8],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+function palyerHasWon(){
+    for(const condition of winningCombos){
+        let [a,b,c]=condition;
+        if(spaces[a] && (spaces[a]==spaces[b]) && (spaces[a]==spaces[c])){
+            return [a,b,c];
+        }
+    }
+    return false;
+}
+
+restartBtn.addEventListener('click', restart)
+
+function restart(){
+    spaces.fill(null);
+    boxes.forEach(box => {
+        box.innerText='';
+        box.style.backgroundColor='';
+    });
+    playerText = 'Tic Tac Toe';
+    currentPlayer = X_TEXT;
+}
+
+startGame()
